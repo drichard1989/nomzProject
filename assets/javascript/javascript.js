fa0781db18244a4ca4812c9ef67350e1969dc340
drichard1989 <drichard1989@icloud.com>
2016-12-10 14:10:50 -0600

    Adding event listener to sign in to google

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -19,6 +19,8 @@ $(document).ready(function(){
 
   var provider = new firebase.auth.GoogleAuthProvider();
 
+  $("#signInDropdown").on("click" function(){
+
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 	  // This gives you a Google Access Token. You can use it to access the Google API.
 	  var token = result.credential.accessToken;
@@ -37,7 +39,13 @@ $(document).ready(function(){
       if (user) {
       	$("#signOut").show();
       	$("#signInDropdown").hide();
-      }
+      };
+
+
+
+  });
+
+
 
      
 	  // ...
