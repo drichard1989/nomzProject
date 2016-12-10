09ad9c0225d4aaeb054fe7d7e44f672a7da3a755
drichard1989 <drichard1989@icloud.com>
2016-12-10 11:44:59 -0600

    Adding a poput for google authentication instead of a redirect

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,16 +15,12 @@ $(document).ready(function(){
 
   var provider = new firebase.auth.GoogleAuthProvider();
 
-firebase.auth().signInWithRedirect(provider);
-
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
+  // ...
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
@@ -34,9 +30,10 @@ firebase.auth().getRedirectResult().then(function(result) {
   // The firebase.auth.AuthCredential type that was used.
   var credential = error.credential;
   // ...
-
-  return false;
 });
+
+
+
 
 
 
