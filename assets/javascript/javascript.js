b8cb316d66d09f62973f44a205a9dfd891d3a19e
drichard1989 <drichard1989@icloud.com>
2016-12-13 16:37:13 -0600

    cleaned up the code for organization.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 
+
+  // All Global Variables
 	var profilePicUrl;
 	var userName;
-  // Initialize Firebase
+  // Variable to Initialize Firebase
   var config = {
     apiKey: "AIzaSyBEsEbAIqr60HleiXsvIcx-lko9l4Bnp6U",
     authDomain: "nomz-5b99c.firebaseapp.com",
@@ -10,7 +12,7 @@ $(document).ready(function(){
     storageBucket: "nomz-5b99c.appspot.com",
     messagingSenderId: "655959060095"
   };
-
+  // Variable for the user object in Firebase that the food is pushed into. 
   var userObject = {
     food: {
 
@@ -20,14 +22,10 @@ $(document).ready(function(){
   firebase.initializeApp(config);
 
   var database = firebase.database();
-  // Creates user directory in Firebase
+
 
 // This all pertains to logging in to Google with Authentication
-
   var provider = new firebase.auth.GoogleAuthProvider();
-
-
-
 	firebase.auth().signInWithPopup(provider).then(function(result) {
 	  // This gives you a Google Access Token. You can use it to access the Google API.
 	  var token = result.credential.accessToken;
@@ -52,7 +50,6 @@ $(document).ready(function(){
         var firebaseUser = database.ref('/users/' + userName);
         firebaseUser.push(userObject);
       };
-
       console.log(userObject);
 	// });
 
@@ -65,52 +62,34 @@ $(document).ready(function(){
  //  	console.error('Sign Out Error', error);
 	// });
  //  });
-
 });
 
-  $("#foodSearchButton").on("click", function(){
 
+  // This currently adds the food that is searched for into the database, not in the user section.
+  $("#foodSearchButton").on("click", function(){
+    // This asks them to enter something if the section is blank when they submit.
     if ($('#foodSearchBox').val().trim() === ""){
       $('#addFoodItemModal').modal();
-        } else {
+        } 
+        // This enters the data into firebase after trimming it.
+        else {
           console.log("food button working")
         // the food from the searchox is trimmed and added 
-
         var foodItem = $("#foodSearchBox").val().trim();
-
-
         // Create local "temporary" object for holding food information
-
-
         var newFood = {
           foodItem: foodItem
         };
-
-
-
         // Uploads new food item to the database
         database.ref(userObject.food).push(newFood);
-
         // Logs the information to the console
         console.log(newFood.foodItem);
         } 
-
   	     // Clear the search boxes
-
   	   $("#foodSearchBox").val("");
 
   	return false;
   }); // closes the foodSearchButton function
-
-
-
-
-
-
-
-
-
-
 
   // function that triggers the submit button when users hit "enter"
   $("#foodSearchBox").keyup(function(event){
